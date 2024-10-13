38f74ba149ce7dd054ad42a507fe16c89696eb3d
MatheusMendes96 <66337016+MatheusMendes96@users.noreply.github.com>
2024-10-13 17:44:22 -0300

    Update b3.py

--- b3.py
+++ b3.py
@@ -18,8 +18,9 @@ tickerDF = empresa.history(period = '1d',
 col1, col2, col3 = st.columns([1,1,1])
 with col1:
     st.write(f"**Empresa:** {empresa.info['longName']}")
-#with col2:
+with col2:
 #    st.write(f"**Mercado:** {empresa.info['industryDisp']}")
+     st.write(f"**Mercado:** {empresa.info['industry']}")
 with col3:
     st.write(f"**Preço Atual:** R$ {empresa.info['currentPrice']}")
 
