51161f12ce0a1925ae620f4c75b8269d7d39915a
MatheusMendes96 <66337016+MatheusMendes96@users.noreply.github.com>
2024-10-13 15:41:01 -0300

    Add files via upload

---
+++ b3.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import yfinance as yf
+
+st.set_page_config(
+    page_title = 'DASHBOARD FINANCEIRO - AÇÕES',
+    layout = 'wide'
+)
+
+st.header('**FECHAMENTO E DIVIDENDO DE AÇÕES**')
+
+ticker = st.text_input('Digite o ticker da ação', 'BBAS3')
+empresa = yf.Ticker(f"{ticker}.SA")
+
+tickerDF = empresa.history(period = '1d', 
+                           start = '2019-01-01',
+                           end = '2024-10-13')
+
+col1, col2, col3 = st.columns([1,1,1])
+with col1:
+    st.write(f"**Empresa:** {empresa.info['longName']}")
+with col2:
+    st.write(f"**Mercado:** {empresa.info['industryDisp']}")
+with col3:
+    st.write(f"**Preço Atual: R$** {empresa.info['currentPrice']}")
+
+st.line_chart(tickerDF.Close)
+st.bar_chart(tickerDF.Dividends)
